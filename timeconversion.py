a1cf10a7e17b5161b2b9a82bf216219c82846990
engelspencer <sengel@sjnma.org>
2017-10-06 13:51:07 -0500

    This program is complete. It has 100% validation. If you input anything incorrect, the program will either correct it for you or spit out an error. All functions to properly convert from civilian to military times or vice versa work 100%.

--- timeconversion.py
+++ timeconversion.py
@@ -3,6 +3,7 @@ conversion = ' '
 splitlist1 = [' ' , ' ']
 splitlist2 = [' ' , ' ']
 milmath = 0
+temptime = ' '
 
 if (':' in time):
 	if ('am' in time or 'Am' in time or 'aM' in time or 'AM' in time or 'a.m' in time or 'A.m' in time or 'a.m' in time or 'A.M' in time or 'am.' in time or 'Am.' in time or 'aM.' in time or 'AM.' in time or 'a.m.' in time or 'A.m.' in time or 'a.M.' in time or 'A.M.' in time):
@@ -34,8 +35,11 @@ if (':' in time):
 			print ('Error: unrecognized format')
 		print (conversion)
 elif ((time[0] == '0' or time[0] == '1' or time[0] == '2') and (time[1] == '0' or time[1] == '1' or time[1] == '2' or time[1] == '3' or time[1] == '4' or time[1] == '5' or time[1] == '6' or time[1] == '7' or time[1] == '8' or time[1] == '9') and (time[2] == '0' or time[2] == '1' or time[2] == '2' or time[2] == '3' or time[2] == '4' or time[2] == '5') and (time[3] == '0' or time[3] == '1' or time[3] == '2' or time[3] == '3' or time[3] == '4' or time[3] == '5' or time[3] == '6' or time[3] == '7' or time[3] == '8' or time[3] == '9')):
-	milmath = int(time)
-	if (milmath >= 0 and milmath < 1000):
+	splitlist1 = time.split()
+	milmath = int(splitlist1[0])
+	if (milmath == 0 or milmath == 2400):
+		conversion = '12:00 am'
+	elif (milmath >= 1 and milmath < 1000):
 		if (milmath < 10):
 			conversion = '12:0' + time[3] + ' am'
 		elif (milmath < 100):
@@ -44,7 +48,20 @@ elif ((time[0] == '0' or time[0] == '1' or time[0] == '2') and (time[1] == '0' o
 			conversion = time[1] + ':'  + time[2] + time[3] + ' am'
 		else:
 			print ('Error: unrecognized format')
-	elif (milmath < 1200 and milmath > 999):
+	elif (milmath >= 1000 and milmath < 1200):
 		conversion = time[0] + time[1] + ':' + time[2] + time[3] + ' am'
+	elif (milmath >= 1200 and milmath < 1300):
+		conversion = time[0] + time[1] + ':' + time[2] + time[3] + ' pm'
+	elif (milmath >= 1300 and milmath < 2400):
+		temptime = str(milmath - 1200)
+		if (milmath < 2200):
+			conversion = temptime[0] + ':' + temptime[1] + temptime[2] + ' pm'
+		elif (milmath >= 2200):
+			conversion = temptime[0] + temptime[1] + ':' + temptime[2] + temptime[3] + ' pm'
+		else:
+			print('Error: unrecognized format')
+	else:
+		print ('Error: unrecognized format')
+	print (conversion)
 else:
 	print ("Error: unrecognized format")
